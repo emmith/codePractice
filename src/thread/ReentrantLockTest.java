18913584eb339651cc57deef6f6744a38f5aba2d
wangrenjun <18873890185@163.com>
2022-04-21 11:22:26 +0800

    对比ReentrantLock 和 Synchronized的效率，虽然Synchronized实现了自旋锁但是效率似乎还是比不上ReentrantLock锁

--- src/thread/ReentrantLockTest.java
+++ src/thread/ReentrantLockTest.java
@@ -3,13 +3,17 @@ package thread;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class WaitAndNotifyTest {
+public class ReentrantLockTest {
+
     private static final int TASK_NUMBER = 10000000;
     private static final int THREAD_NUMBER = 4;
 
     public static void main(String[] args) throws InterruptedException {
-        var ts = new TaskQueue();
+        var ts = new TaskQueueReenTrantLock();
         var tl = new ArrayList<Thread>();
 
         for (int i = 0; i < THREAD_NUMBER; i++) {
@@ -34,7 +38,7 @@ public class WaitAndNotifyTest {
 //                System.out.printf("add %s \n", task);
                 ts.addTask(task);
 //                try {
-//                    Thread.sleep(100);
+//                    Thread.sleep(1);
 //                } catch (InterruptedException e) {
 //                    e.printStackTrace();
 //                }
@@ -47,36 +51,36 @@ public class WaitAndNotifyTest {
         for (var task : tl) {
             task.interrupt();
         }
-        //6543ms
+        //2411ms
         System.out.println(System.currentTimeMillis() - start + "ms");
-
     }
 }
 
-class TaskQueue {
+class TaskQueueReenTrantLock {
+    private final Lock lock = new ReentrantLock();
     Queue<String> queue = new LinkedList<>();
+    private final Condition condition = lock.newCondition();
 
-    public synchronized void addTask(String s) {
-        this.queue.add(s);
-        //唤醒调用了wait进入等待状态的线程
-        this.notifyAll();
+    public void addTask(String s) {
+        if (lock.tryLock()) {
+            try {
+                queue.add(s);
+                condition.signalAll();
+            } finally {
+                lock.unlock();
+            }
+        }
     }
 
-    public synchronized String getTask() throws InterruptedException {
-        /*
-         * 这里为什么要用while循环，而不是if呢
-         * 因为notifyAll是一次唤醒所有线程，换成if是直接往下执行
-         * 而加入队列已经被其他线程搬空，就会报错
-         * 用while是为了再判断一次队列是否为空
-         *
-         * 为什么不用notify呢，notify是唤醒一个，不能最大的利用资源
-         */
-        while (this.queue.isEmpty()) {
-            //获得锁以后发现没有任务，就会释放锁，进入等待状态，等待notify唤醒
-            //被唤醒后需要重新获得锁
-            this.wait();
+    public String getTask() throws InterruptedException {
+        lock.lock();
+        try {
+            while (queue.isEmpty()) {
+                condition.await();
+            }
+            return queue.remove();
+        } finally {
+            lock.unlock();
         }
-
-        return queue.remove();
     }
 }
